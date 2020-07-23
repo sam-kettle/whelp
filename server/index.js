9db7c7f568da10b1d3d485c94528fe037d15c3f0
Sam Eddy <sam-kettle@outlook.com>
2020-07-23 18:29:46 +0100

    Server now fetches hardcoded Yelp URL

--- server/index.js
+++ server/index.js
@@ -8,6 +8,23 @@ const PORT = process.env.PORT || 5000;
 
 // Basic route
 app.get("/", (req, res) => {
+    const getData = async () => {
+        try {
+            const url =
+                "https://api.yelp.com/v3/businesses/search?term=barbers&location=Birmingham";
+            const config = {
+                headers: {
+                    Authorization: "Bearer " + process.env.YELP_API_KEY,
+                },
+            };
+            const result = await axios.get(url, config);
+            const data = result.data;
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    getData();
     res.send("Express backend running.");
 });
 
