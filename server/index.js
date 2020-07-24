f8472c982564c8c9bfbdbc494694aca16d52ae5f
Sam Eddy <samueljeddy@gmail.com>
2020-07-24 11:44:06 +0100

    Server accepts location and business type

--- server/index.js
+++ server/index.js
@@ -7,23 +7,26 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Basic route
-app.get("/", (req, res) => {
-    const getData = async () => {
+app.get("/:term/:location", (req, res) => {
+    const term = req.params.term;
+    const location = req.params.location;
+
+    const getBusinesses = async () => {
         try {
             const url =
-                "https://api.yelp.com/v3/businesses/search?term=barbers&location=Birmingham";
+                `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`;
             const config = {
                 headers: {
                     Authorization: "Bearer " + process.env.YELP_API_KEY,
                 },
             };
             const result = await axios.get(url, config);
-            return result.data;
+            return result.data.businesses;
         } catch (e) {
             console.log(e);
         }
     };
-    getData().then((data) => {
+    getBusinesses().then((data) => {
         res.json(data);
     });
 });
